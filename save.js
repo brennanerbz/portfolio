9271d856db6a5a8928591a056e7ee72e27654a59
brennanerbz <brennan.erbeznik@gmail.com>
2016-06-06 11:38:54 -0700

    Mon Jun  6 11:38:54 PDT 2016

--- save.js
+++ save.js
@@ -1,4 +1,2 @@
-// var cmd = require('node-cmd');
-// cmd.run('git add --all && git commit -m \"$(date)\" && git push')
 const exec = require('child_process').exec;
 exec('git add --all && git commit -m \"$(date)\" && git push')
