73ef7d6d2755a2bc8435a5266aa7ef2ba4fe30a7
brennanerbz <brennan.erbeznik@gmail.com>
2016-06-06 14:52:06 -0700

    added nav tabs and routes

---
+++ src/components/link.js
@@ -0,0 +1,48 @@
+import React  from 'react';
+
+export default React.createClass({
+	displayName: 'Link',
+
+	getInitialState() {
+		return {
+			active: false
+		}
+	},
+
+	componentDidMount(props) {
+		const {active} = this.props;
+		if(active) this.setState({
+			active: true
+		});
+	},
+
+	componentWillReceiveProps(nextProps) {
+		const {active} = this.props;
+		const nextActive = nextProps.active;
+		if(!active && nextActive) {
+			this.setState({
+				active: true
+			}) 
+		} else if (active && !nextActive) {
+			this.setState({
+				active: false
+			}) 
+		}
+	},
+
+	render() {
+		const { active } = this.state;
+		const { className } = this.props;
+		const { activeClass } = this.props;
+		const { idleClass } = this.props;
+		const { label } = this.props;
+		const { to } = this.props;
+		return(
+			<a className={className + ' ' + (active ? activeClass : idleClass)} 
+				href={to}
+				onClick={() => this.props.handleChangeRoute(to)}>
+				{label}
+			</a>
+		);
+	}
+});
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,12 +1,71 @@
 import React  from 'react';
+import app from 'ampersand-app';
+import Link from './link';
 
 export default React.createClass({
 	displayName: 'Nav',
 
+	getInitialState() {
+		return {
+			activeRoute: app.router.history.location.pathname
+		}	
+	},
+
+	componentDidMount() {
+		const pathname = app.router.history.location.pathname;
+		this.setState({
+			activeRoute: pathname
+		});
+	},
+
+	changeRoute(pathname) {
+		this.setState({
+			activeRoute: pathname
+		});
+	},
+
 	render() {
+		const { activeRoute } = this.state;
 		return(
 			<nav className="nav">
-				
+				<div className="container group">
+					<div className="float-left nav-logo">
+					</div>
+					<div className="float-right nav-links">
+						<ul className="links flat margin-small mr">
+							<li>
+								<Link 
+									className="margin-med mr padding-tiny pb"
+									label="Work" 
+									to="/" 
+									active={activeRoute === '/'}
+									idleClass="grey"
+									activeClass="black border bb br"
+									handleChangeRoute={this.changeRoute}/>
+							</li>
+							<li>
+								<Link 
+									className="padding-tiny pb"
+									label="About" 
+									to="/about" 
+									active={activeRoute === '/about'}
+									idleClass="grey"
+									activeClass="black border bb br"
+									handleChangeRoute={this.changeRoute}/>
+							</li>
+							<li>
+								<Link 
+									className="margin-med ml padding-tiny pb"
+									label="Hire Me" 
+									to="/hire-me" 
+									active={activeRoute === '/hire-me'}
+									idleClass="grey"
+									activeClass="black border bb br"
+									handleChangeRoute={this.changeRoute}/>
+							</li>
+						</ul>
+					</div>
+				</div>
 			</nav>
 		);
 	}
